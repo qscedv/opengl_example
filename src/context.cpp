c256eb4f641a71b66ee680ea7218ac29fc6684d6
이주영 <diane1019@naver.com>
2021-05-20 16:30:02 +0900

    그라데이션 사각형 그리기

--- src/context.cpp
+++ src/context.cpp
@@ -10,11 +10,12 @@ ContextUPtr Context::Create() {
 bool Context::Init() {
 
     float vertices[] = {
-         0.5f,  0.5f, 0.0f, // top right
-         0.5f, -0.5f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f, // bottom left
-        -0.5f,  0.5f, 0.0f, // top left
+         0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 1.0f, // top right, white
+         0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom right, blue
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, // bottom left, black
+        -0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // top left, red
     };
+
     uint32_t indices[] = { // note that we start from 0!
         0, 1, 3, // first triangle
         1, 2, 3, // second triangle
@@ -23,15 +24,17 @@ bool Context::Init() {
     m_vertexLayout = VertexLayout::Create();
 
     m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER,
-        GL_STATIC_DRAW, vertices, sizeof(float) * 12);
+        GL_STATIC_DRAW, vertices, sizeof(float) * 24);
     
-    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+    // m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
+    m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, sizeof(float) * 3);
 
     m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER,
         GL_STATIC_DRAW, indices, sizeof(uint32_t) * 6);
  
-    ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
-    ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
+    ShaderPtr vertShader = Shader::CreateFromFile("./shader/per_vertex_color.vs", GL_VERTEX_SHADER);
+    ShaderPtr fragShader = Shader::CreateFromFile("./shader/per_vertex_color.fs", GL_FRAGMENT_SHADER);
     if (!vertShader || !fragShader)
         return false;
     SPDLOG_INFO("vertex shader id: {}", vertShader->Get());
@@ -41,7 +44,7 @@ bool Context::Init() {
     if (!m_program)
         return false;
     SPDLOG_INFO("program id: {}", m_program->Get());
-
+ 
     glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
 
     return true;
