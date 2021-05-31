be9d0e97e582afb4c4285bedabd33f99326a23e1
이주영 <diane1019@naver.com>
2021-05-31 12:28:09 +0900

    Blinn Shading, Shadow Map, Normal Mapping

--- shader/lighting.fs
+++ shader/lighting.fs
@@ -17,6 +17,7 @@ struct Light {
     vec3 specular;
 };
 uniform Light light;
+uniform int blinn;
  
 //재질
 struct Material {
@@ -49,11 +50,18 @@ void main() {
         vec3 diffuse = diff * texColor * light.diffuse;
 
         vec3 specColor = texture2D(material.specular, texCoord).xyz;
-        vec3 viewDir = normalize(viewPos - position);
-        vec3 reflectDir = reflect(-lightDir, pixelNorm);
-        float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+        float spec = 0.0;
+        if (blinn == 0) {
+            vec3 viewDir = normalize(viewPos - position);
+            vec3 reflectDir = reflect(-lightDir, pixelNorm);
+            spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+        }
+        else {
+            vec3 viewDir = normalize(viewPos - position);
+            vec3 halfDir = normalize(lightDir + viewDir);
+            spec = pow(max(dot(halfDir, pixelNorm), 0.0), material.shininess);
+        }
         vec3 specular = spec * specColor * light.specular;
-
         result += (diffuse + specular) * intensity;
     }
 
