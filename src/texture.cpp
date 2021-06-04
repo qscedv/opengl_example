61854a628ed408e9f287e0e59a1befe36cfd42cd
이주영 <diane1019@naver.com>
2021-06-04 19:01:25 +0900

    Deferred shading, SSAO

--- src/texture.cpp
+++ src/texture.cpp
@@ -46,9 +46,30 @@ void Texture::SetTextureFormat(int width, int height, uint32_t format, uint32_t 
     m_format = format;
     m_type = type;
 
+    GLenum imageFormat = GL_RGBA;
+    if (m_format == GL_DEPTH_COMPONENT) {
+        imageFormat = GL_DEPTH_COMPONENT;        
+    }
+    else if (m_format == GL_RGB ||
+        m_format == GL_RGB16F ||
+        m_format == GL_RGB32F) {
+        imageFormat = GL_RGB;
+    }
+    else if (m_format == GL_RG ||
+        m_format == GL_RG16F ||
+        m_format == GL_RG32F) {
+        imageFormat = GL_RG;
+    }
+    else if (m_format == GL_RED ||
+        m_format == GL_R ||
+        m_format == GL_R16F ||
+        m_format == GL_R32F) {
+        imageFormat = GL_RED;
+    }
+    
     glTexImage2D(GL_TEXTURE_2D, 0, m_format,
         m_width, m_height, 0,
-        m_format, m_type,
+        imageFormat, m_type,
         nullptr);
 }
 
